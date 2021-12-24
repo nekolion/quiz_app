1e119d6ff4a9386cf09bfc6ad92d091081e8b657
nekolion <50746807+nekolion@users.noreply.github.com>
2021-12-25 08:23:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,89 +1,38 @@
-const quiz =[
-  {
-    question : '作業を細分化し、階層構造にして管理する手法は？',
-    answers : ['WBS','AWS','RHA','AES'],
-    correct: 'WBS'
-  },
-  {
-    question : 'パスワードの可能な組み合わせをすべて試す総当たり攻撃と呼ばれるものは？',
-    answers : ['類推攻撃','レインボー攻撃','ブルートフォース攻撃','リバートブルートフォース攻撃'],
-    correct: 'ブルートフォース攻撃'
-  },
-  {
-    question : '組織内データが外部に情報漏洩することを防ぐための製品の名称は？',
-    answers : ['DMZ','DLP','BMP','DNP'],
-    correct: 'DLP'
-  },
-  {
-    question : 'ハッカーの説明で正しいものは？',
-    answers : ['悪意があって攻撃してくる人','善悪かかわらず高い技術を持つ人','システムを破壊しようとしてくる人','パソコンに詳しい人'],
-    correct: '善悪かかわらず高い技術を持つ人'
-  },
-  {
-    question : 'いたずら目的などで既存のツールを使ってサイバー攻撃する攻撃者は？',
-    answers : ['クラッカー','スクリプトキティ','愉快犯','ハッカー'],
-    correct: 'スクリプトキティ'
-  },
-  {
-    question : 'コンピューターウイルスの潜伏機能で正しいものはどれ？',
-    answers : ['脆弱性を悪用して侵入してくる機能','ファイルの破壊を行う機能・意図しない動作をする機能','条件が整うまで発病しない機能','プログラムやファイルにウイルスを感染していく機能'],
-    correct: '条件が整うまで発病しない機能'
-  },
-  {
-    question : '公開鍵は、不特定多数の利用者に公開する。では、秘密鍵は？',
-    answers : ['誰にも公開しない','相手に公開する','条件によっては教えてあげる。','そんなものは存在しない'],
-    correct: '誰にも公開しない'
-  },
-  {
-    question : 'パスワードをもとに利用者IDを試す攻撃方法は？',
-    answers : ['パスワードリスト攻撃','レインボー攻撃','ブルートフォース攻撃','リバートブルートフォース攻撃'],
-    correct: 'リバートブルートフォース攻撃'
-  }
-]
-
-const quizLength = quiz.length;
-let quizIndex = 0;
-
-let score = 0;
-
-
-const $button = document.getElementsByTagName('button') //よく使う変数
-const buttonLength = $button.length;
-
-//変数の文字列をHTMLに反映させる
-const setUpQuiz = () => {
-document.getElementById('js-question').textContent = quiz[quizIndex].question;
-let buttonIndex = 0;
-while(buttonIndex < buttonLength){
-  $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
- buttonIndex++;
-} 
-}
-setUpQuiz();
-
-//ボタンをクリックした時の正誤判定
-const clickHandler = (e) => {
-  if(quiz[quizIndex].correct === e.target.textContent){
-    window.alert('正解');
-    score++;
-  }else{
-    window.alert('不正解');
-  }
-
-  quizIndex++;
-  if(quizIndex < quizLength){
-    //問題があれば実行
-    setUpQuiz();
-  }else{
-  //問題がなければ実行
-  window.alert("終了です。あなたの正解数は"+quizLength+"個中　"+score+"個です");
-}};
-
-
-let handlerIndex = 0;
-while(handlerIndex < buttonLength){
-  $button[handlerIndex].addEventListener('click', (e) => {
-    clickHandler(e);
-  })
- handlerIndex++;
-};
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>暗記用クイズゲーム</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+    
+    <div class="jumbotron mt-5">
+      <div class="d-flex justify-content-center">
+        <div id="js-question" class="alert alert-primary" role="alert">
+          JSうまく読み込めてないよ。
+        </div>
+      </div>
+      
+      <div id="js-items" class="d-flex justify-content-center">
+        <div class="m-2">
+          <button type="button" id="js-btn-1" class="btn btn-primary">読み込めてないよ</button>
+        </div>
+        <div class="m-2">
+          <button type="button" id="js-btn-2" class="btn btn-primary">読み込めてないよ</button>
+        </div>
+        <div class="m-2">
+          <button type="button" id="js-btn-3" class="btn btn-primary">読み込めてないよ</button>
+        </div>
+        <div class="m-2">
+          <button type="button" id="js-btn-4" class="btn btn-primary">読み込めてないよ</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="app.js"></script>
+</body>
+</html>
